ac4b2e0b58fb3e752de488b268883c4978f95ea5
loky13 <loky13@daum.net>
2023-04-17 22:29:31 +0900

    spring boot + vue 개발 세팅
    vue dev 시 proxy 설정으로 8081 -> 8080 으로 설정
    tomcat 기본 8080 명시적으로 작성
    options.js -> vue inspect > options.js

--- JPA/1_DEV_APPLICATION/jpashop/front-end/vue.config.js
+++ JPA/1_DEV_APPLICATION/jpashop/front-end/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //build output directory
+  outputDir: "../back-end/src/main/resources/static",
+  //build index directory
+  indexPath: "../static/index.html",
+
+  //dev server // npm run serve options
+  devServer: {
+    //proxy
+    proxy: {
+      // http://locahost:8081 -> http://localhost:8080
+      '/': {
+        target: 'http://localhost:8080',
+        //cross origin
+        changeOrigin: true,
+      }
+    }
+  }
 })
