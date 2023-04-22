d202a5c02b4aaae6a35b72db137b6800e95a6055
loky13 <loky13@daum.net>
2023-04-22 14:57:47 +0900

    회원 api test code 작성

--- JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/repository/MemberRepository.java
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/repository/MemberRepository.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
---
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/test/java/jpaBook/jpaShop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpaBook.jpaShop.service;
+
+import jpaBook.jpaShop.domain.Member;
+import jpaBook.jpaShop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Fail.fail;
+
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void  회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when 
+        Long savedId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
+    }
+    
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = new Member();
+        member1.setName("kim1");
+
+        Member member2 = new Member();
+        member2.setName("kim1");
+
+        //when 
+        memberService.join(member1);
+        //then
+        Assertions.assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//        Assertions.fail("예외가 발생해야 합니다.");
+    }
+}
