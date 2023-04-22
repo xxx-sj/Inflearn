3860fcfd987b2504e0bf25ae0d145795cc4d2ab4
loky13 <loky13@daum.net>
2023-04-22 22:21:25 +0900

    조건 조회 querySQL 추가 전

--- JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/repository/OrderRepository.java
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/repository/OrderRepository.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -20,5 +23,43 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+
+        return em.createQuery("select o from Order o join o.member m" +
+                " where o.status = :status" +
+                " and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)
+                .getResultList();
+
+    }
+
+    /**
+     * JPA Criteria
+     * 권장 x
+     * 단점: 유지보수가 떨어진다.
+     */
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<Order> query = criteriaBuilder.createQuery(Order.class);
+
+        Root<Order> o = query.from(Order.class);
+        Join<Object, Object> m = o.join("member", JoinType.INNER);
+
+        List<Predicate> criteria = new ArrayList<>();
+
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = criteriaBuilder.equal(o.get("status"), orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //....
+
+        return null;
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+
+//    }
 }
---
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpaBook.jpaShop.repository;
+
+import jpaBook.jpaShop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus; //주문 상태 [ORDER, CANCEL]
+}
