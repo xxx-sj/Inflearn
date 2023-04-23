1459ebe971be88d0db2a0d25c9ff1cccce8d9bc3
loky13 <loky13@daum.net>
2023-04-24 01:30:56 +0900

    erorr controller 추가

---
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/controller/error/JpaShopErrorController.java
@@ -0,0 +1,14 @@
+package jpaBook.jpaShop.controller.error;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JpaShopErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String redirectRoot() {
+        return "index.html";
+    }
+}
