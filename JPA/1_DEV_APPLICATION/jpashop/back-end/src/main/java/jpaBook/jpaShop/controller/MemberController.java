638654f39b31479ef356f747966ec1736f86706e
loky13 <loky13@daum.net>
2023-04-26 00:56:09 +0900

    memberFormController 추가

---
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/controller/MemberController.java
@@ -0,0 +1,31 @@
+package jpaBook.jpaShop.controller;
+
+import jpaBook.jpaShop.domain.Address;
+import jpaBook.jpaShop.domain.Member;
+import jpaBook.jpaShop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("rest/api/v1/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/register")
+    public void create(@Valid MemberForm form) {
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+    }
+}
