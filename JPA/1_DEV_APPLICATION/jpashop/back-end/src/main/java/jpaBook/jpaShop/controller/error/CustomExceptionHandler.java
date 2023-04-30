d758ebe4790d4ca429dfe4b905e1ab741a56f78c
loky13 <loky13@daum.net>
2023-04-30 15:32:51 +0900

    오류핸들러 주석제거

--- JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/controller/error/CustomExceptionHandler.java
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/controller/error/CustomExceptionHandler.java
@@ -5,11 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class CustomExceptionHandler {
 
-//    @ExceptionHandler(IllegalStateException.class)
-//    public ResponseEntity<String> handlerException(IllegalStateException e) {
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
-//    }
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handlerException(IllegalStateException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.toString());
+    }
 }
