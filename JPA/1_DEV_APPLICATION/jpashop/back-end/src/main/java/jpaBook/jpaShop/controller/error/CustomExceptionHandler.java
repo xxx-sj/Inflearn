c30ecac12c2b9678be6965ff8a274a2308355a4b
loky13 <loky13@daum.net>
2023-05-01 22:38:51 +0900

    memberForm validation exception handler 추가

--- JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/controller/error/CustomExceptionHandler.java
+++ JPA/1_DEV_APPLICATION/jpashop/back-end/src/main/java/jpaBook/jpaShop/controller/error/CustomExceptionHandler.java
@@ -2,6 +2,7 @@ package jpaBook.jpaShop.controller.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,5 +12,10 @@ public class CustomExceptionHandler {
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handlerException(IllegalStateException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.toString());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> argValidateException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getFieldError().getDefaultMessage());
     }
 }
