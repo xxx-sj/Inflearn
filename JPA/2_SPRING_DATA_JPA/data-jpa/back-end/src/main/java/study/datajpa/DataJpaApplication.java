29041a6fb7ac0d88668b3d453087e78cce9c6b95
loky13 <loky13@daum.net>
2023-05-20 23:55:13 +0900

    공통 인터페이스 설명 spring-data-jpa
    team repo 를 jpa repo로 생성

--- JPA/2_SPRING_DATA_JPA/data-jpa/back-end/src/main/java/study/datajpa/DataJpaApplication.java
+++ JPA/2_SPRING_DATA_JPA/data-jpa/back-end/src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,8 +2,10 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository")
 public class DataJpaApplication {
 
 	public static void main(String[] args) {
