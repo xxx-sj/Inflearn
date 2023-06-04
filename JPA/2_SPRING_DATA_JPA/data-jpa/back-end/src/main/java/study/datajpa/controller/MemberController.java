1b1150de9af94cd4e1db2252abb1f2bd7fe34a65
loky13 <loky13@daum.net>
2023-06-04 11:02:21 +0900

    native query

--- JPA/2_SPRING_DATA_JPA/data-jpa/back-end/src/main/java/study/datajpa/controller/MemberController.java
+++ JPA/2_SPRING_DATA_JPA/data-jpa/back-end/src/main/java/study/datajpa/controller/MemberController.java
@@ -40,7 +40,7 @@ public class MemberController {
     @PostConstruct
     public void init() {
         for (int i = 0; i < 100; i++) {
-            memberRepository.save(new Member("user" + 1, i));
+//            memberRepository.save(new Member("user" + 1, i));
         }
 //        memberRepository.save(new Member("userA"));
     }
--- JPA/2_SPRING_DATA_JPA/data-jpa/back-end/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ JPA/2_SPRING_DATA_JPA/data-jpa/back-end/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -321,4 +321,30 @@ class MemberRepositoryTest {
 //            System.out.println("usernameOnly = " + usernameOnly.getAge());
         }
     }
+
+    @Test
+    public void nativeQuery() {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+
+        Member m1 = new Member("m1", 0, teamA);
+        Member m2 = new Member("m2", 0, teamA);
+        em.persist(m1);
+        em.persist(m2);
+
+        em.flush();
+        em.clear();
+
+        //where
+//        Member result = memberRepository.findByNativeQuery("m1");
+        Page<MemberProjection> result = memberRepository.findByNativeProjection(PageRequest.of(0, 10));
+        List<MemberProjection> content = result.getContent();
+
+        for (MemberProjection memberProjection : content) {
+            System.out.println("memberProjection = " + memberProjection.getUsername());
+            System.out.println("memberProjection = " + memberProjection.getTeamName());
+        }
+
+    }
 }
