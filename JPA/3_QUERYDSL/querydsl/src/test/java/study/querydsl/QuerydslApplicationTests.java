ba661338ef7b55da4f97734060c1961c83521e77
loky13 <loky13@daum.net>
2023-06-04 22:53:51 +0900

    querydsl setting 및 예제 entity 설정

---
+++ JPA/3_QUERYDSL/querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,40 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+//		QHello qHello = new QHello("h");
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
