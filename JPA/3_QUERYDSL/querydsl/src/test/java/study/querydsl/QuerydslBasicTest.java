dbbc65bda684aa3c983241f428e13a78bc2fe1b0
loky13 <loky13@daum.net>
2023-06-06 01:40:54 +0900

    queryProjection

--- JPA/3_QUERYDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ JPA/3_QUERYDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -593,5 +594,17 @@ public class QuerydslBasicTest {
                 .from(member)
                 .fetch();
 
+    }
+
+    @Test
+    public void findDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
     }
 }
