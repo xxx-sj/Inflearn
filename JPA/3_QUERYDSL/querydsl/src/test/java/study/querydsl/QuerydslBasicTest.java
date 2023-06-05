dce156ab00d8cb2fabf90a9b608c020c1f774e14
loky13 <loky13@daum.net>
2023-06-05 22:19:13 +0900

    상수, 문자 더하기

--- JPA/3_QUERYDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ JPA/3_QUERYDSL/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assert;
@@ -455,6 +456,36 @@ public class QuerydslBasicTest {
         for (String s : reuslt) {
             System.out.println("s = " + s);
 
+        }
+    }
+
+    //상수 추가
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    //문자 더하기
+    //age는 int형이기에 string으로 변경
+    @Test
+    public void concat() {
+
+        //{username}_{age}
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
         }
     }
 }
