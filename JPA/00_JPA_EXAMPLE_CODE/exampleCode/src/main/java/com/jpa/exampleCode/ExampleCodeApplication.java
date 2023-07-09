7afd11dc294f405a98c17638bf5bf2f1eda0dcc2
loky13 <loky13@daum.net>
2023-07-09 15:04:10 +0900

    init
    jpa example code repo

---
+++ JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/main/java/com/jpa/exampleCode/ExampleCodeApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.exampleCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleCodeApplication.class, args);
+	}
+
+}
---
+++ JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/test/java/com/jpa/exampleCode/ExampleCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.exampleCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
