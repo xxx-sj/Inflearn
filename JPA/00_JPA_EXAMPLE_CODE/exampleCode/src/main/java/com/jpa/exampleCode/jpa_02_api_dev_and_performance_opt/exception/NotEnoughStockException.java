483863e1c7ff77d851806475b3d5aa03dfde8c4f
loky13 <loky13@daum.net>
2023-10-04 23:49:03 +0900

    exception 추가

---
+++ JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/main/java/com/jpa/exampleCode/jpa_02_api_dev_and_performance_opt/exception/NotEnoughStockException.java
@@ -0,0 +1,22 @@
+package com.jpa.exampleCode.jpa_02_api_dev_and_performance_opt.exception;
+
+
+public class NotEnoughStockException extends RuntimeException {
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+
+}
