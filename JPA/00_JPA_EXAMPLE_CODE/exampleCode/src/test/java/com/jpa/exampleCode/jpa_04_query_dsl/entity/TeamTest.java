74a31cffe46247427265a93f314efec625545df0
loky13 <loky13@daum.net>
2023-11-17 23:57:17 +0900

    실전! querydsl: 섹션2 예제 도메인 모델: 예제 도메인 모델과 동작확인

---
+++ JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/test/java/com/jpa/exampleCode/jpa_04_query_dsl/entity/TeamTest.java
@@ -0,0 +1,55 @@
+package com.jpa.exampleCode.jpa_04_query_dsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import javax.persistence.EntityManager;
+import javax.transaction.TransactionScoped;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class TeamTest {
+
+    @Autowired
+    EntityManager em;
+
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("member.getTeam() = " + member.getTeam());
+        }
+    }
+
+}
---
+++ JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/test/java/com/jpa/exampleCode/jpa_04_query_dsl/section_01_project_setting/HelloTest.java
@@ -0,0 +1,36 @@
+package com.jpa.exampleCode.jpa_04_query_dsl.section_01_project_setting;
+
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class HelloTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        Assertions.assertEquals(result, hello);
+        Assertions.assertEquals(result.getId(), hello.getId());
+    }
+}
