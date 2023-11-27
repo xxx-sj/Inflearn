c79a66558f5a62d80423b1cda306d59104309789
loky13 <loky13@daum.net>
2023-11-27 13:54:16 +0900

    실전! querydsl : 섹션5 - 동적 쿼리와 성능 최적화 조회 - builder사용

--- JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/main/java/com/jpa/exampleCode/jpa_04_query_dsl/repository/MemberJpaRepository.java
+++ JPA/00_JPA_EXAMPLE_CODE/exampleCode/src/main/java/com/jpa/exampleCode/jpa_04_query_dsl/repository/MemberJpaRepository.java
@@ -1,11 +1,18 @@
 package com.jpa.exampleCode.jpa_04_query_dsl.repository;
 
 
+import com.jpa.exampleCode.jpa_04_query_dsl.dto.MemberSearchCondition;
+import com.jpa.exampleCode.jpa_04_query_dsl.dto.MemberTeamDto;
+import com.jpa.exampleCode.jpa_04_query_dsl.dto.QMemberDto;
+import com.jpa.exampleCode.jpa_04_query_dsl.dto.QMemberTeamDto;
 import com.jpa.exampleCode.jpa_04_query_dsl.entity.Member;
 import com.jpa.exampleCode.jpa_04_query_dsl.entity.QMember;
+import com.jpa.exampleCode.jpa_04_query_dsl.entity.QTeam;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -55,6 +62,39 @@ public class MemberJpaRepository {
         return queryFactory
                 .selectFrom(member)
                 .where(member.username.eq(username))
+                .fetch();
+    }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+
+        if(StringUtils.hasText(condition.getTeamName())) {
+            builder.and(QTeam.team.name.eq(condition.getTeamName()));
+        }
+
+        if(condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+
+        if(condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        QTeam.team.id.as("teamId"),
+                        QTeam.team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, QTeam.team)
+                .where(builder)
                 .fetch();
     }
 }
