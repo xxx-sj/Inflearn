39bb9cd8687a93324e5df4d227fd5190278f7312
상진 <loky13@daum.net>
2024-02-14 01:04:03 +0900

    유틸리티 객체와 날짜

--- spring/spring_boot/04.스프링MVC2편-백엔드_웹_개발_핵심_기술/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ spring/spring_boot/04.스프링MVC2편-백엔드_웹_개발_핵심_기술/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -53,6 +54,13 @@ public class BasicController {
     public String basicObjects(HttpSession session) {
         session.setAttribute("sessionData", "Hello Session");
         return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+
+        return"basic/date";
     }
 
     @Component("helloBean")
