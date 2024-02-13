70b5eb56603f19ed40d9dfdfbe156f99ab31d6e2
상진 <loky13@daum.net>
2024-02-14 00:59:13 +0900

    기본객체들

--- spring/spring_boot/04.스프링MVC2편-백엔드_웹_개발_핵심_기술/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ spring/spring_boot/04.스프링MVC2편-백엔드_웹_개발_핵심_기술/thymeleaf-basic/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,6 +1,8 @@
 package hello.thymeleafbasic.basic;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,6 +47,19 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
