e74126117f07f78840b0161ef14fcd5842da3fc4
상진 <loky13@daum.net>
2024-02-14 17:22:30 +0900

    요구사항 추가

---
+++ spring/spring_boot/04.스프링MVC2편-백엔드_웹_개발_핵심_기술/form/src/main/java/hello/itemservice/domain/DeliveryCode.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain;
+
+/**
+ * FAST: 빠른 배송
+ * NORMAL: 일반 배송
+ * SLOW: 느린 배송
+ */
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+}
